782350275f586a27631ff6898c2c3f40504f3672
Mahak Kansal <mahakkansal47@gmail.com>
2026-06-22 14:57:54 +0530

    Add files via upload

---
+++ Assignment 1/1.js
@@ -0,0 +1,40 @@
+//question1//
+let a=12
+let b=20
+result=a+b
+console.log(result);
+
+//question2//
+console.log(a<10&&a>b);
+
+//question3//
+let c=100
+let d=45
+console.log((c%10==0|| d%10==0)? 'true':'false');
+
+//question4//
+let f=2345
+res=(Math.floor(f/1000))
+console.log(res);
+
+//question5//
+console.log(f%10)
+
+//question6//
+let v=18
+let w=4
+resl=v%w
+console.log(resl);
+
+//question7//
+resl=v*w
+console.log(resl);
+
+//question8//
+let x=60
+let y=56
+let z=90
+let total=x+y+z
+console.log(total);
+let average=(x+y+z)/3
+console.log(average);
