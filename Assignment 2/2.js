e96f7eb04e6d4cf2a6a2110a2c7e23fc8fe70281
Mahak Kansal <mahakkansal47@gmail.com>
2026-06-24 14:46:09 +0530

    Add files via upload

---
+++ Assignment 2/2.js
@@ -0,0 +1,89 @@
+//question 1
+//let marks=parseInt (prompt("enter marks"));
+// switch(true){
+//     case  (marks <= 10):
+//     console.log('E');
+//     break
+//     case  (marks>11&&marks<20):
+//     console.log('D');
+//     break
+//     case  (marks>21&& marks<30):
+//     console.log('C');
+//     break
+//     case  (marks>31&& marks<40):
+//     console.log('B');
+//     break
+//     case  (marks>41&& marks<50):
+//     console.log('A');
+//     break
+// }
+
+//question 2
+let M = 'd';
+    switch (M) {
+        case 'P':
+        case 'p':
+            console.log("PrepBytes") ;
+        break
+        case 'Z':
+        case 'z':
+            console.log("Zenith");
+        break
+        case 'E':
+        case 'e':
+            console.log("Expert Coder");
+            break
+
+        case 'D':
+        case 'd':
+            console.log("Data Structure");
+            break
+        default:
+        console.log("Invalid Character");
+    }
+    
+
+
+//question3
+// let A=239
+// let B=768
+// let C=9876
+// if(A>=0,B>=0,C>=0){
+//     if(A>B && A>C){ 
+//         console.log("A");
+// }
+// else if(B>A && B>C){ 
+//         console.log("B");
+// }
+// else if(C>A && C>B){ 
+//         console.log("C");
+// }
+// else {
+//     console.log("-1");
+// }
+// }
+
+// //question4
+// let X=23
+// let Y=14
+// let Z=69
+// if ((X > Y && X < Z) || (X > Z && X < Y)){
+//         console.log("X");
+// }
+//     else if ((Y > X && Y < Z) || (Y > Z && Y < X)){
+//         console.log("Y");
+//     }
+//     else{
+//         console.log("Z");
+//     }
+
+// //question 5
+// let angle1 = 45
+// let angle2=80
+// let angle3=20
+// if(angle1<90||angle2<90||angle3 <90){
+//     console.log("Acute");
+// }
+// else{
+//     console.log("Obtuse");
+// }
